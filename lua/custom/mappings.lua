71b08450dc5f12b857376f7c1c2e562d459361dd
zhangzelin <zhangzelin@baidu.com>
2023-05-16 08:52:29 +0800

    init custom

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,64 @@
+local M = {}
+
+M.mine = {
+  n = {
+    ["<S-s>"] = {":w<CR>", "Save file"},
+    ["<S-q>"] = {":q<CR>", "Quit file"},
+    ["sd"] = {":set splitright<CR> :vsplit<CR>", "split right"},
+    ["=="] = {"$", "last token", noremap = true },
+    ["--"] = {"0", "fist token", noremap = true },
+    ["<LEADER><RIGHT>"] = {"<C-w>l", "Move to right window"},
+    ["<LEADER><LEFT>"] = {"<C-w>h", "Move to left window"},
+    ["<LEADER><DOWN>"] = {"<C-w>j", "Move to below window"},
+    ["<LEADER><UP>"] = {"<C-w>k", "Move to upper window"},
+    ["tp"] = {":noh<CR>", "ignore select section"},
+  }
+}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<LEADER>db"] = {
+      "<CMD> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line"
+    },
+    ["<LEADER>dus"] = {
+      function ()
+        local widgets = require("dap.ui.widgets")
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open()
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
+
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<LEADER>dgt"] = {
+      function ()
+        require('dap-go').debug_test()
+      end,
+      "Debug go test"
+    },
+    ["<LEADER>dgl"] = {
+      function ()
+        require('dap-go').debug_last()
+      end,
+      "Debug last go test"
+    }
+  }
+}
+
+M.go = {
+  plugin = true,
+  n = {
+    ["<LEADER>gtj"] = {
+      ":GoTagAdd json<CR>",
+      "add json tag for struct",
+    }
+  }
+}
+
+return M
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,54 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      }
+    }
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function ()
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    init = function ()
+      require("core.utils").load_mappings("go")
+    end,
+    config = function (_, opts)
+      require("dap-go").setup(opts)
+    end
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function (_,opts)
+      require("gopher").setup(opts)
+    end,
+
+    build = function ()
+      vim.cmd [[slient! GoInstallDeps]]
+    end
+
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end,
+  }
+}
+return plugins
